b324cc73d3fcb3f6106a70b5ffefd2a8c1f614c2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-01 12:38:57 +0000

    Deploy libmbd/libmbd.github.io to github.com/libmbd/libmbd.github.io.git:master

--- pymbd/_static/documentation_options.js
+++ pymbd/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.0.post4.dev0+9d00e18',
+    VERSION: '0.12.0.post6.dev0+299a5e2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
